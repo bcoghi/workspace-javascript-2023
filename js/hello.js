e19a6b60d051ed7e5de83a848d7e07bb969718e4
bcoghi <bcoghill601@gmail.com>
2024-10-21 13:12:16 -0400

    practice oct21

--- js/hello.js
+++ js/hello.js
@@ -1,7 +1,7 @@
 
 /* Send a customized message based on time
 */
-//console.log("I am in the hello.js");
+console.log("I am in the hello.js");
 
 // get the date and time 
 var today = new Date();
@@ -10,9 +10,13 @@ var hoursNow = today.getHours();
 // display the greeting message 
 // modify the code so that your code display "Good afternoon" whereas time is from 12:00pm to 6:00pm
 var greetings = "";
-if (hoursNow <= 12){
+if (hoursNow < 12){
   greetings = "Good Morning";
-}else{
+}
+else if (hoursNow < 18){
+  greetings = "Good Afternoon";
+}
+else{
   greetings = "Good Evening";
 }
 
